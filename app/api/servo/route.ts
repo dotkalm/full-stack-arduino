fc7c30c5f76f28165538e0ba8cd8c43797ed6b85
Claire Kwong <clairebkwong@gmail.com>
2025-11-11 17:18:50 -0500

    turn servo to a random angle each time

--- app/api/servo/route.ts
+++ app/api/servo/route.ts
@@ -3,24 +3,17 @@ import { boardManager } from "../../lib/board-manager";
 
 export async function GET() {
   try {
-    console.log("Motor!");
-
     // Just check if already initialized, don't re-initialize
     if (!boardManager.isInitialized()) {
       return new Response("Board not ready yet", { status: 503 });
     }
 
-    // Now you can safely use the board
-    const board = boardManager.getBoard();
-
-    console.log("Motor board callback!");
-    //const angle = Math.floor(Math.random() * 360);
-    //console.log("call servo! " + angle);
+    const angle = Math.floor(Math.random() * 360);
+    console.log("call servo! " + angle);
     const servo = new five.Servo(10);
-    //servo.to(angle);
-    servo.sweep();
+    servo.to(angle);
 
-    return new Response("Hello from Johnny Five API! Sweeping!");
+    return new Response("Success - Servo!");
   } catch (err) {
     console.error(err);
     return new Response("Error initializing Johnny Five", { status: 500 });
